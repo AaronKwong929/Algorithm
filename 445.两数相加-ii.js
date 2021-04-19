01b828dc1182a0d4d43d5aaabb2e5c49de0a4263
AaronKwong929 <AaronKwong929@gmail.com>
2021-04-19 17:46:37 +0800

    32132

--- 445.两数相加-ii.js
+++ 445.两数相加-ii.js
@@ -22,37 +22,6 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    // const stack1 = [],
-    //     stack2 = [],
-    //     list = [],
-    //     dummy = new ListNode(0);
-    // let cur = dummy;
-    // while (l1) {
-    //     stack1.push(l1.val);
-    //     l1 = l1.next;
-    // }
-    // while (l2) {
-    //     stack2.push(l2.val);
-    //     l2 = l2.next;
-    // }
-    // let carry = 0;
-    // while (stack1.length > 0 || stack2.length > 0) {
-    //     const num1 = stack1.pop() || 0,
-    //         num2 = stack2.pop() || 0,
-    //         sum = num1 + num2 + carry;
-    //     // js记得parseInt 不然有个0.0xxxxxx
-    //     carry = parseInt(sum / 10);
-    //     const target = parseInt(sum % 10);
-    //     list.push(target);
-    // }
-
-    // // 容易忽略的边界条件：最终结果存在进位
-    // if (carry !== 0) list.push(carry);
-    // while (list.length > 0) {
-    //     cur.next = new ListNode(list.pop());
-    //     cur = cur.next;
-    // }
-    // return dummy.next;
     const stack1 = [],
         stack2 = [],
         dummy = new ListNode(0);
