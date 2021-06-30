08776313417d95e34f8e01dbac037ae9b6ccbeb7
AaronKwong929 <AaronKwong929@gmail.com>
2021-06-30 11:53:29 +0800

    123

---
+++ 剑指Offer-10-I.斐波那契数列.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+    // const res = [0, 1];
+    // for (let i = 2; i <= n; i++) {
+    //     res[i] = (res[i - 1] + res[i - 2]) % 1000000007;
+    // }
+    // return res[n];
+};
+
+console.log(fib(5));
+
+// 头递归，记得题目有说值要求模
