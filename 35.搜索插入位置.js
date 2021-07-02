8f6c48e005583f02ffc698e5036f6fda9af7b959
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-02 11:27:04 +0800

    35 ok

--- 35.搜索插入位置.js
+++ 35.搜索插入位置.js
@@ -50,14 +50,16 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-    if (nums[nums.length - 1] < target) return nums.length;
-    if (nums[0] > target) return 0;
+    if (target < nums[0]) return 0;
+    if (target > nums[nums.length - 1]) return nums.length;
     let left = 0,
         right = nums.length - 1;
-    while (left < right) {
+    while (left <= right) {
         const mid = left + Math.floor((right - left) / 2);
         if (nums[mid] < target) left = mid + 1;
-        else right = mid;
+        else {
+            right = mid - 1;
+        }
     }
     return left;
 };
