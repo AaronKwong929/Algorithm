3bc9de1817d1816aaec5d84b118e0531620684d2
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-02 11:08:16 +0800

    704 ok

--- 704.二分查找.js
+++ 704.二分查找.js
@@ -54,7 +54,7 @@ var search = function (nums, target) {
         right = nums.length - 1;
     while (left <= right) {
         const mid = left + Math.floor((right - left) / 2);
-        if (target === nums[mid]) return mid;
+        if (nums[mid] === target) return mid;
         else if (nums[mid] < target) left = mid + 1;
         else right = mid - 1;
     }
