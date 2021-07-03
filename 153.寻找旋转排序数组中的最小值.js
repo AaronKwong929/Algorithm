b4c5decd06865bb9be5cff466e90a2de66e541fe
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-03 11:02:25 +0800

    12312312

--- 153.寻找旋转排序数组中的最小值.js
+++ 153.寻找旋转排序数组中的最小值.js
@@ -71,27 +71,17 @@
  * @return {number}
  */
 var findMin = function (nums) {
-    if (nums[0] < nums[nums.length - 1]) return nums[0];
+    const { length } = nums;
     let left = 0,
-        right = nums.length - 1;
+        right = length - 1;
+    if (nums[0] < nums[length - 1]) return nums[0];
     while (left <= right) {
-        const mid = left + Math.floor((right - left) / 2);
-
-        if (nums[mid] > nums[mid + 1]) {
-            return nums[mid + 1];
-        }
-
-        if (nums[mid] < nums[mid - 1]) {
-            return nums[mid];
-        }
-
-        if (nums[mid] > nums[left]) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+        const mid = left + ((right - left) >> 1);
+        if (nums[mid] < nums[mid - 1]) return nums[mid];
+        if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
+        if (nums[mid] > nums[left]) left = mid + 1;
+        else right = mid - 1;
     }
-
     return nums[left];
 };
 // @lc code=end
--- 剑指Offer-11.旋转数组的最小数字.js
+++ 剑指Offer-11.旋转数组的最小数字.js
@@ -2,7 +2,20 @@
  * @param {number[]} numbers
  * @return {number}
  */
-var minArray = function (numbers) {};
+var minArray = function (numbers) {
+    const { length } = numbers;
+    let left = 0,
+        right = length - 1;
+    if (numbers[0] < numbers[length - 1]) return numbers[0];
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        if (numbers[mid] < numbers[mid - 1]) return numbers[mid];
+        if (numbers[mid] > numbers[mid + 1]) return numbers[mid + 1];
+        if (numbers[mid] > numbers[left]) left = mid + 1;
+        else right = mid - 1;
+    }
+    return numbers[left];
+};
 
 // O(n)
 // 优解：升序数组的断层处即是最小值 [4,5,6,1,2,3] 456升序，断层1是最小值
