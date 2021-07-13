f007875fef8a6b335040cc1479a7d73a9000dbd7
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-13 16:08:53 +0800

    545646

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -17,15 +17,40 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-    if (!head || head.next === null) return head;
-    let p = head.next;
-    head.next = null; // 头节点断开 head: 1 ->null
-    while (p !== null) {
-        const temp = p.next; // temp 3-4-5-null
-        p.next = head; // p: 2-1-null
-        head = p; // head 2-1-null
-        p = temp; // p 3-4-5-null
+    let pre = null,
+        cur = head,
+        next = head;
+    while (cur !== null) {
+        next = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = next;
+    }
+    return pre;
+
+    if (!head || !head.next) return head;
+    let next = head.next;
+    head.next = null;
+    while (next !== null) {
+        const temp = next.next;
+        next.next = head;
+        head = next;
+        next = temp;
     }
     return head;
+
+    // 递归
+    if (!head || !head.next) return head;
+    let last = null;
+    const helper = head => {
+        if (!head.next) return head;
+        last = helper(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    };
+    return helper(head);
 };
 // @lc code=end
+
+// https://labuladong.gitbook.io/algo/mu-lu-ye-1/mu-lu-ye/di-gui-fan-zhuan-lian-biao-de-yi-bu-fen
---
+++ 92.反转链表-ii.js
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=javascript
+ *
+ * [92] 反转链表 II
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list-ii/description/
+ *
+ * algorithms
+ * Medium (54.41%)
+ * Likes:    948
+ * Dislikes: 0
+ * Total Accepted:    180.7K
+ * Total Submissions: 332.2K
+ * Testcase Example:  '[1,2,3,4,5]\n2\n4'
+ *
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left  。请你反转从位置 left 到位置 right 的链表节点，返回
+ * 反转后的链表 。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5], left = 2, right = 4
+ * 输出：[1,4,3,2,5]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [5], left = 1, right = 1
+ * 输出：[5]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点数目为 n
+ * 1
+ * -500
+ * 1
+ *
+ *
+ *
+ *
+ * 进阶： 你可以使用一趟扫描完成反转吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function (head, left, right) {
+    let successor = null;
+    let last = null;
+    const helper = (head, n) => {
+        if (n === 1) {
+            successor = head.next;
+            return head;
+        }
+        last = helper(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    };
+
+    if (left === 1) {
+        return helper(head, right);
+    }
+    head.next = reverseBetween(head.next, left - 1, right - 1);
+    return head;
+};
+// @lc code=end
+
+// https://labuladong.gitbook.io/algo/mu-lu-ye-1/mu-lu-ye/di-gui-fan-zhuan-lian-biao-de-yi-bu-fen
