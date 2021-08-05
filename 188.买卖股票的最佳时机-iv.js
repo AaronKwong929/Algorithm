2955c0a4a0e8a3bd6dccd8403896875810b356a7
AaronKwong929 <AaronKwong929@gmail.com>
2021-08-05 17:19:26 +0800

    lll

--- 188.买卖股票的最佳时机-iv.js
+++ 188.买卖股票的最佳时机-iv.js
@@ -14,39 +14,39 @@
  * Testcase Example:  '2\n[2,4,1]'
  *
  * 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
- * 
+ *
  * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
- * 
+ *
  * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：k = 2, prices = [2,4,1]
  * 输出：2
  * 解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
- * 
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：k = 2, prices = [3,2,6,5,0,3]
  * 输出：7
  * 解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
  * ⁠    随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 =
  * 3 。
- * 
- * 
- * 
+ *
+ *
+ *
  * 提示：
- * 
- * 
- * 0 
- * 0 
- * 0 
- * 
- * 
+ *
+ *
+ * 0
+ * 0
+ * 0
+ *
+ *
  */
 
 // @lc code=start
@@ -55,8 +55,56 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(k, prices) {
+var maxProfit = function (max_k, prices) {
+    const { length } = prices;
+    if (length === 0) return 0; // 注意
+    if (max_k > length / 2) return maxProfit_k_inf(prices);
+    const dp = new Array(length)
+        .fill([])
+        .map(() =>
+            new Array(max_k + 1).fill([]).map(() => new Array(2).fill(0))
+        ); // 注意
 
+    for (let i = 0; i < length; i++) {
+        for (let k = max_k; k >= 1; k--) {
+            if (i === 0) {
+                dp[i][k][0] = 0;
+                dp[i][k][1] = -prices[i];
+                continue;
+            }
+            dp[i][k][0] = Math.max(
+                dp[i - 1][k][0],
+                dp[i - 1][k][1] + prices[i]
+            );
+
+            dp[i][k][1] = Math.max(
+                dp[i - 1][k][1],
+                dp[i - 1][k - 1][0] - prices[i]
+            );
+        }
+    }
+    return dp[length - 1][max_k][0];
+};
+const maxProfit_k_inf = prices => {
+    const { length } = prices;
+    let dp_i_0 = 0,
+        dp_i_1 = -Infinity;
+    for (let i = 0; i < length; i++) {
+        let temp = dp_i_0;
+        dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+        dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+    }
+    return dp_i_0;
 };
 // @lc code=end
-
+console.log(maxProfit(2, []));
+/**
+ * base case
+ * i - 1 === -1    i === 0
+ * dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i])
+ *             = max(0, -Infinity + price[i])
+ *             = 0
+ * dp[i][k][1] = max(dp[i - 1][k][1], dp[i - 1][k][0] - price[i])
+ *             = max(-Infinity, 0-price[i])
+ *             = -price[i]
+ */
--- 309.最佳买卖股票时机含冷冻期.js
+++ 309.最佳买卖股票时机含冷冻期.js
@@ -14,20 +14,20 @@
  * Testcase Example:  '[1,2,3,0,2]'
  *
  * 给定一个整数数组，其中第 i 个元素代表了第 i 天的股票价格 。​
- * 
+ *
  * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
- * 
- * 
+ *
+ *
  * 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
  * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
- * 
- * 
+ *
+ *
  * 示例:
- * 
+ *
  * 输入: [1,2,3,0,2]
- * 输出: 3 
+ * 输出: 3
  * 解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
- * 
+ *
  */
 
 // @lc code=start
@@ -35,8 +35,31 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-
+var maxProfit = function (prices) {
+    const { length } = prices;
+    let dp_i_0 = 0,
+        dp_i_1 = -Infinity;
+    for (let i = 0; i < length; i++) {
+        let temp = dp_i_0;
+        let dp_pre_0 = 0; // dp[i - 2][0]
+        dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+        dp_i_1 = Math.max(dp_i_1, dp_pre_0 - prices[i]);
+        dp_pre_0 = temp;
+    }
+    return dp_i_0;
 };
 // @lc code=end
 
+/**
+ * 有冷冻期
+ *
+ * 先状态转移方程
+ * dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i])
+ *             = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+ *
+ * dp[i][k][1] = max(dp[i - 1][k][1], dp[i - 2][k][0] - prices[i])
+ *             = max(dp[i - 1][1], dp[i - 2][0] - prices[i])
+ * sell 之后要等一天才能交易，所以再次购买的时候需要从 i - 2 开始算
+ * temp = dp_i_0
+ * 然后 dp_pre_0 参与计算，计算完成后才有 dp_pre_0 = temp 实现跨天 
+ */
